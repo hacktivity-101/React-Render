8356e66d506d6fc5b9004a7a63c28d375f8f01dd
m-triple-m <triplem656@gmail.com>
2024-04-29 19:02:51 +0530

    ..

--- templating-engine/src/expressions/constants.jsx
+++ templating-engine/src/expressions/constants.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const constants = ({ constants }) => {
   return (
     <div className="constant-tab">
-    <h3>Constants</h3>
-    <ul>
-      {constants.map((constant, index) => (
-        <li key={index}>{constant.name}: {constant.value}</li>
-      ))}
-    </ul>
-  </div>  )
+      <h3>Constants</h3>
+      <ul>
+        {constants.map((constant, index) => (
+          <li key={index}>{constant.name}: {constant.value}</li>
+        ))}
+      </ul>
+    </div>)
 }
 
 export default constants
--- templating-engine/src/expressions/layout.jsx
+++ templating-engine/src/expressions/layout.jsx
@@ -1,55 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-class ExpressionTab extends React.Component {
-    render() {
-        return (
-            <div className="expression-tab">
-                <div className="tab-layout">
-                    <ul className="tabs">
-                        <li className="tab active">Tab 1</li>
-                        <li className="tab">Tab 2</li>
-                        <li className="tab">Tab 3</li>
-                    </ul>
-                    <div className="tab-content">
-                        <ExpressionColumn />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+const Layout = ({children}) => {
+  return (
+    <div>Layout</div>
+  )
 }
 
-class ExpressionColumn extends React.Component {
-    render() {
-        return (
-            <table className="expression-column">
-                <thead>
-                    <tr>
-                        <th>Column 1</th>
-                        <th>Column 2</th>
-                        <th>Column 3</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Data 1</td>
-                        <td>Data 2</td>
-                        <td>Data 3</td>
-                    </tr>
-                    <tr>
-                        <td>Data 4</td>
-                        <td>Data 5</td>
-                        <td>Data 6</td>
-                    </tr>
-                    <tr>
-                        <td>Data 7</td>
-                        <td>Data 8</td>
-                        <td>Data 9</td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
-}
-
-export default ExpressionTab;
+export default Layout
