b3e0b8172f17b16ec1c2104975e258d0a7ea726c
m-triple-m <triplem656@gmail.com>
2024-05-01 18:25:42 +0530

    ..

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const Model = require ('../models/userModel');
+const Model = require('../models/userModel');
 
 const router = express.Router();
 
-router.post('/add', (req.res) => {
-    new Model (req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-       console.log(err);
-       res.status(500).json(err); 
-    });
+router.post('/add', (req, res) => {
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/getall',(req, res) => {
+router.get('/getall', (req, res) => {
     Model.find()
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 //: denotes url parameter
-router.delete('/delete/:id', (req, res) =>{
-     Model.findByIDDelete(req.params.id)
-     .then((result) => {
-        res.status(200).json(result);
-     }).catch((err) => {
-        console.log(err);
-       res.status(500).json(err);
-     });
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIDDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
 
 module.exports = router;
