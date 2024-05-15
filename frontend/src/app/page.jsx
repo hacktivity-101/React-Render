e794c8060c2c7112e43a27fb92f0f16f495fc347
hacktivity-101 <maddyakash2136@gmail.com>
2024-05-15 13:18:26 +0530

    quick-start

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -156,7 +156,7 @@ const Home = () => {
               </p>
               <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                 <a
-                  href="https://github.com/themesberg/landwind"
+                  href="#"
                   className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                 >
                   <svg
@@ -170,7 +170,7 @@ const Home = () => {
                   View on GitHub
                 </a>
                 <a
-                  href="#"
+                  href="/docs/quick-start"
                   className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                 >
                   <svg
@@ -188,8 +188,8 @@ const Home = () => {
                           ".st0{fill:#0acf83}.st1{fill:#a259ff}.st2{fill:#f24e1e}.st3{fill:#ff7262}.st4{fill:#1abcfe}"
                       }}
                     />
-                    <title>Figma.logo</title>
-                    <desc>Created using Figma</desc>
+                    <title>React-Render</title>
+                    <desc>Created using React-Render</desc>
                     <path
                       id="path0_fill"
                       className="st0"
@@ -216,7 +216,7 @@ const Home = () => {
                       d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"
                     />
                   </svg>{" "}
-                  Get Figma file
+                  Get-Started
                 </a>
               </div>
             </div>
