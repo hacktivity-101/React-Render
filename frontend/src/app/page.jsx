2c73d353bb00f983d22ec6fb1010365d1411c4f6
hacktivity-101 <maddyakash2136@gmail.com>
2024-05-15 15:16:00 +0530

    n mn

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -156,7 +156,7 @@ const Home = () => {
               </p>
               <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                 <a
-                  href="#"
+                  href="https://github.com/hacktivity-101/templating-engine   "
                   className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                 >
                   <svg
