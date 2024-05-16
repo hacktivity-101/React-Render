d7645bcb481410c4225eabb0a01157763f402306
hacktivity-101 <maddyakash2136@gmail.com>
2024-05-16 10:11:03 +0530

    ............

--- frontend/src/app/docs/about/page.jsx
+++ frontend/src/app/docs/about/page.jsx
@@ -9,7 +9,9 @@ const About = () => {
         <section className='mb-10'>
           <h2 className='text-3xl font-bold'>What is React-Render</h2>
           <hr className='mt-2 mb-6 border-slate-400' />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis excepturi dolore molestias maxime ratione! Animi dolore accusamus a quos minima ipsa iste quo, iure enim recusandae, vel est nisi soluta praesentium deserunt mollitia! Reiciendis, pariatur excepturi! Blanditiis quam quaerat illum.</p>
+          <p>A React-Render allows you to generate HTML dynamically using a template. React-Render uses predefined templates with placeholder variables that are then replaced with dynamic data. 
+Existing templating engines often require developers to mix HTML with complex JavaScript syntax, leading to convoluted code. This complicates readability, maintainability, and hampers the separation of concerns, hindering efficient development and collaboration. React breaks away from the classic templating model by introducing a component-based structure and the power of JSX. In React, JSX serves as a templating engine where you can write HTML-like syntax directly without JavaScript.
+</p>
             
           
         </section>
